b6763e70936bc13904d32e9269ba109fdc4424ad
Jaben <jaben@yetanotherforum.net>
2013-12-10 22:13:48 -0500

    Fixed Issue #6026 -- Supports "Minimize on Close" option.
    (cherry picked from commit d0ba2e426f5588a79d00c18d346464df8e866f7e)
    
    Conflicts:
    	src/Papercut/App.config
    	src/Papercut/Properties/Settings.Designer.cs
    	src/Papercut/Properties/Settings.settings
    	src/Papercut/UI/MainWindow.xaml
    	src/Papercut/UI/MainWindow.xaml.cs
    	src/Papercut/UI/OptionsWindow.xaml
    	src/Papercut/UI/OptionsWindow.xaml.cs

--- src/Papercut/Properties/Settings.Designer.cs
+++ src/Papercut/Properties/Settings.Designer.cs
@@ -106,5 +106,17 @@ namespace Papercut.Properties {
                 this["ShowDefaultTab"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool MinimizeOnClose {
+            get {
+                return ((bool)(this["MinimizeOnClose"]));
+            }
+            set {
+                this["MinimizeOnClose"] = value;
+            }
+        }
     }
 }
