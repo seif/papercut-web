598d94ec5fff55924573820002705136ae1e4c18
Mike Hook <hook.mike@gmail.com>
2014-02-07 11:32:13 +0000

    Return a promise so durandal gets all the data from the async response. Ref: http://stackoverflow.com/questions/15083516/how-to-use-observables-in-durandal

--- src/Papercut.WebHost/App/viewmodels/email.js
+++ src/Papercut.WebHost/App/viewmodels/email.js
@@ -1,28 +1,32 @@
-﻿define(['durandal/http', 'durandal/app'], function(http, app) {
+﻿define(['durandal/http', 'durandal/app'], function (http, app) {
 
-    return {
-        displayName: 'Email',
-    
-        Id: ko.observable(),
-        From: ko.observable(),
-        To: ko.observable(),
-        Subject: ko.observable(),
-        Body: ko.observable(),
-        Date: ko.observable(),
-        activate: function (params) {
-            var $this = this;
-            var url = 'email/' + encodeURIComponent(params.id);
-            $.getJSON(url).success(function (data) {
-                $this.Id(data.Id);
-                $this.From(data.From);
-                $this.To(data.To);
-                $this.Subject(data.Subject);
-                $this.Body(data.Body);
-                $this.Date(data.Date);
-            }).error(function (e) {
-                console.log(e);
-            });
-        }
-    };
+	return {
+		displayName: 'Email',
+
+		Id: ko.observable(),
+		From: ko.observable(),
+		To: ko.observable(),
+		Subject: ko.observable(),
+		Body: ko.observable(),
+		Date: ko.observable(),
+		activate: function (params) {
+			var self = this;
+			var url = 'email/' + encodeURIComponent(params.id);
+
+			//Return a promise so durandal gets all the data from the async response. Ref: http://stackoverflow.com/questions/15083516/how-to-use-observables-in-durandal
+			var promise = $.getJSON(url).success(function (data) {
+				self.Id(data.Id);
+				self.From(data.From);
+				self.To(data.To);
+				self.Subject(data.Subject);
+				self.Body(data.Body);
+				self.Date(data.Date);
+			}).error(function (e) {
+				console.log(e);
+			});
+
+			return promise;
+		}
+	};
 });
    
--- src/Papercut.WebHost/App/viewmodels/mailbox.js
+++ src/Papercut.WebHost/App/viewmodels/mailbox.js
@@ -1,41 +1,43 @@
 ﻿define(['durandal/http', 'durandal/app'], function (http, app) {
-    var router = require('durandal/plugins/router');
+	var router = require('durandal/plugins/router');
 
-    return {
-        displayName: "Mailbox",
-        emails: ko.observableArray([]),
-        links: ko.observableArray([]),
-        currentPageNumber: ko.observable(1),
-        totalPages: ko.observable(1),
+	return {
+		displayName: "Mailbox",
+		emails: ko.observableArray([]),
+		links: ko.observableArray([]),
+		currentPageNumber: ko.observable(1),
+		totalPages: ko.observable(1),
 
-        activate: function (args) {
-            var page = args.splat ? args.splat[0] : 1;
-            this.getEmails(page);
-        },
+		activate: function (args) {
+			var page = args.splat ? args.splat[0] : 1;
 
-        open: function (email) {
-            router.navigateTo('#/email/' + encodeURIComponent(email.Id), 'skip');
-            email.viewUrl = 'views/email';
-            app.showModal(email).then(function () {
-                router.navigateBack();
-            });
-        },
 
-        getEmails: function (page) {
-            var that = this;
-            router.navigateTo('#/mailbox/page/' + page, 'skip');
-            http.get('emails/', {
-                page: page,
-                format: 'json'
-            }).then(function (data) {
-                setTimeout(function() {
-                    that.emails(data.Emails);
-                    console.log(that.emails().length);
-                    that.currentPageNumber(data.Page);
-                    that.totalPages(data.Pages);
-                }, 500);
+			return this.getEmails(page);
+		},
 
-            });
-        }
-    };
+		open: function (email) {
+			router.navigateTo('#/email/' + encodeURIComponent(email.Id), 'skip');
+			email.viewUrl = 'views/email';
+			app.showModal(email).then(function () {
+				router.navigateBack();
+			});
+		},
+
+		getEmails: function (page) {
+			var that = this;
+			router.navigateTo('#/mailbox/page/' + page, 'skip');
+
+			var url = 'emails/?format=json&page=' + page
+
+			//Return a promise so durandal gets all the data from the async response. Ref: http://stackoverflow.com/questions/15083516/how-to-use-observables-in-durandal
+			var promise = $.getJSON(url).success(function (data) {
+				that.emails(data.Emails);
+				console.log(that.emails().length);
+				that.currentPageNumber(data.Page);
+				that.totalPages(data.Pages);
+			});
+
+			return promise;
+		}
+	};
 });
